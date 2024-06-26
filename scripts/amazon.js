3b21cfbf77b2705eff1bd67a3855f5ae9046495a
Giao U <lesliegiao.u@gmail.com>
2024-06-26 09:42:13 -0500

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -77,6 +77,13 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
       });
     }
 
-    console.log(cart);
+    let cartQuantity = 0;
+
+    //calculate total of the cart
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+
+    document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
   });
 });
