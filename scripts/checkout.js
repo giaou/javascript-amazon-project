4c94b89fe338bdb16c378d27e386caa2d0a14009
Giao U <lesliegiao.u@gmail.com>
2024-06-26 14:13:34 -0500

    Delete products from HTML when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -17,7 +17,9 @@ cart.forEach((cartItem) => {
   console.log(matchingProduct);
 
   cartSummaryHtml += `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${
+      matchingProduct.id
+    }">
         <div class="delivery-date">
             Delivery date: Tuesday, June 21
         </div>
@@ -107,6 +109,9 @@ document.querySelectorAll(".js-delete-link").forEach((link) => {
     //delete
     removeFromCart(productId);
 
-    console.log(cart);
+    const container = document.querySelector(
+      `.js-cart-item-container-${productId}`
+    );
+    container.remove();
   });
 });
