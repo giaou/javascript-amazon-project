e745efbfe25bc8d61d45a2d1e593c6f7a056332d
Giao U <lesliegiao.u@gmail.com>
2024-06-26 13:20:25 -0500

    import cart.js and product.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,3 +1,6 @@
+import { addToCart, cart } from "../data/cart.js";
+import { products } from "../data/products.js";
+
 let prodcutsHtml = "";
 
 products.forEach((product) => {
@@ -56,34 +59,21 @@ products.forEach((product) => {
 
 document.querySelector(".js-products-grid").innerHTML = prodcutsHtml;
 
+function updateCartQuantity() {
+  let cartQuantity = 0;
+  //calculate total of the cart
+  cart.forEach((item) => {
+    cartQuantity += item.quantity;
+  });
+
+  document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+}
+
+
 document.querySelectorAll(".js-add-to-cart").forEach((button) => {
   button.addEventListener("click", () => {
     const productId = button.dataset.productId;
-
-    let matchingItem;
-
-    cart.forEach((item) => {
-      if (productId === item.productId) {
-        matchingItem = item;
-      }
-    });
-
-    if (matchingItem) {
-      matchingItem.quantity += 1;
-    } else {
-      cart.push({
-        productId: productId,
-        quantity: 1,
-      });
-    }
-
-    let cartQuantity = 0;
-
-    //calculate total of the cart
-    cart.forEach((item) => {
-      cartQuantity += item.quantity;
-    });
-
-    document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+    addToCart(productId);
+    updateCartQuantity();
   });
 });
